ab9853d5727c02f17532193db627eb7006213b79
Ademola <ademolaogunmokun@yahoo.com>
2022-08-26 11:51:08 +0100

    add global style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from './Components/Header';
 function App() {
   return (
-    
+    <div>
+      <Header />
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GblobalStyle } from './Styles/Global.style';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GblobalStyle/>
     <App />
   </React.StrictMode>
 );
