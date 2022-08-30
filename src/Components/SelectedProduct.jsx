b8eba4f92122be972f20a4c0a80522b3bc0b8a3a
Ademola <ademolaogunmokun@yahoo.com>
2022-08-30 11:16:08 +0100

    change  position correctly on the selected product by setting negative margin

--- src/Components/SelectedProduct.jsx
+++ src/Components/SelectedProduct.jsx
@@ -11,7 +11,7 @@ export default function SelectedProduct() {
 
       <div className='cartcontent'>
 
-
+Your cart is empty
 
       </div>
     </SelectedProductStyle>
--- src/Styles/SelectedProduct.style.js
+++ src/Styles/SelectedProduct.style.js
@@ -3,20 +3,24 @@ import styled from 'styled-components';
 export const SelectedProductStyle = styled.div`
 
 background-color: white;
-height: 200px;
-width: 340px;
+height: 300px;
+width: 345px;
 position: relative;
 bottom: 400px;
 margin-right: auto;
 margin-left: auto;
 box-shadow: 0 0 10px 0 rgba(0,0,0,0.2);
 border-radius: 10px;
-margin-bottom: -200px;
+margin-bottom: -300px;
 
 .header {
   font-weight: bold;
   font-size: 1.2em;
   border-bottom: 2px solid hsl(220deg 39% 87%);
   padding: 30px;
+}
+
+.cartcontent{
+  padding: 100px;
 }
 `;
