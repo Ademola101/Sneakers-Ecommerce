30fdc52f3d233f9b6e106e74e3b332436e0fe24b
Ademola <ademolaogunmokun@yahoo.com>
2022-09-02 22:01:01 +0100

    add cursor pointer to icons

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -15,7 +15,9 @@ export default function Modal({ isOpen, handleClose }) {
 
     <ReactModal isOpen = {isOpen} ariaHideApp={false}  className = 'content' overlayClassName= 'Overlay'>
 
-      <svg onClick={handleClose} width="14" height="15" xmlns="http://www.w3.org/2000/svg"><path d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z" fill="#69707D" fillRule="evenodd"/></svg>
+      <svg style={ {
+        marginLeft:370
+      }} onClick={handleClose} width="14" height="15" xmlns="http://www.w3.org/2000/svg"><path d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z" fill="#69707D" fillRule="evenodd"/></svg>
 
 
       <img className='img' src= {displayImage[index]} alt="" />
