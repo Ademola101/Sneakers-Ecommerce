71e53d1c364444db62dbd9c101094e619faee6c1
Ademola <ademolaogunmokun@yahoo.com>
2022-09-11 16:11:47 +0100

    resize layout

--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -5,8 +5,10 @@ import { useSelector } from 'react-redux';
 import NextLeft from './NextLeft';
 import NextRight from './RightNext';
 import Thumbnail from './Thumbnail';
-import Modal from './Modal';
+import Modal from '@mui/material/Modal';
 import { useMediaQuery } from '../Hooks';
+import { Typography, Box } from '@mui/material';
+
 
 
 
@@ -22,28 +24,37 @@ export default function Item() {
 
 
 
-  return (<>
+  return (<div>
     <ItemStyle>
-      <AnimatePresence>
-        <div onClick={() => {
 
-          isMobbile && setisOpen(true);}
-        } >
-          <motion.img className='image'  src={products.find(
-            product => product.id === index+1)?.img} alt="sneakers" initial={{ opacity: 0, y: 0 }
-          }
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          key =  {products.find(
-            product => product.id === index+1)?.img}/>
-        </div>
-        <Thumbnail />
+      <NextLeft />
+      <NextRight/>
+
+
+      <Modal
+        open={isOpen}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+
+
+        <Box >
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+      Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+      Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+      <AnimatePresence>
+        <motion.img className='image' key={index} src={products[index].img} alt={products[index].title}
+          onClick={() => { isMobbile && setisOpen(!isOpen);}} />
+        <Thumbnail/>
+
       </AnimatePresence>
     </ItemStyle>
-    <NextLeft />
-    <NextRight/>
-
-    <Modal isOpen={isOpen} handleClose = {handleClose}/>
-  </>
+  </div>
   );
 }
--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useSelector, } from 'react-redux';
-import ReactModal from 'react-modal';
-import '../Styles/Modal.css';
 import Thumbnail from './Thumbnail';
-
-export default function Modal({ isOpen, handleClose }) {
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+export default function ModalComponent({ open }) {
 
 
   const products = useSelector(state => state.products);
@@ -12,23 +13,9 @@ export default function Modal({ isOpen, handleClose }) {
   const displayImage = products.map(product => product.img);
 
   return (
-
-    <ReactModal isOpen = {isOpen} ariaHideApp={false}  className = 'content' overlayClassName= 'Overlay'>
-
-      <svg style={ {
-        marginLeft:370
-      }} onClick={handleClose} width="14" height="15" xmlns="http://www.w3.org/2000/svg"><path d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z" fill="#69707D" fillRule="evenodd"/></svg>
-
-
-      <img className='img' src= {displayImage[index]} alt="" />
-      <Thumbnail />
-      <div className='svg'>
-        <svg width="12" height="18" xmlns="http://www.w3.org/2000/svg">
-          <path d="M11 1 3 9l8 8" stroke="#1D2026" strokeWidth="3" fill="none" fillRule="evenodd"/></svg>
-
-
-      </div>
-    </ReactModal>
+    <div>
+      <Button >Open modal</Button>
+    </div>
 
   );
 }
