a171f4ae48ff1c0992a57c024ebdfcd47bd5c99d
Ademola <ademolaogunmokun@yahoo.com>
2022-09-27 10:09:41 +0100

    remove notification on mobile

--- src/Styles/Notification.styles.js
+++ src/Styles/Notification.styles.js
@@ -1,8 +1,16 @@
 import styled from 'styled-components';
 export const NotificationStyle  = styled.div`
-display: flex;
+display: none;
 justify-content: center;
 align-items: center;
+
+@media screen and (min-width: 768px) {
+  display: flex;
+justify-content: center;
+align-items: center;
+color: green;
+
+}
 
 
 `;
